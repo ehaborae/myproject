354d51fbda51b295be41e71269f668bbf77baa31
Ehab Borae Hassanien <68748080+ehaborae@users.noreply.github.com>
2022-03-22 14:42:21 +0200

    flex example

--- App.js
+++ App.js
@@ -6,16 +6,34 @@ export default function App() {
 
   return (
     <View style={
-      {padding:20}
+      { padding: 50, flexDirection: 'row',width:'100%',height:500 ,justifyContent:'space-evenly',alignItems:'stretch'}
     }>
-      <Button title='My First Button' />
-      <View >
-        <TextInput />
-        <Text>
-        Ehab
-        </Text>
+      <View style={{
+        flex:1,
+        backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }} >
+        <Text>1</Text>
       </View>
-      <Button title='My First Button' />
+      <View style={{
+        flex:2,
+        backgroundColor: 'blue',
+        width: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }} >
+        <Text>1</Text>
+      </View>
+      <View style={{
+        backgroundColor: 'green',
+        width: 20,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }} >
+        <Text>1</Text>
+      </View>
     </View>
   );
 }
