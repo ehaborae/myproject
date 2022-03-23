07b118e9ec61f972da4d27436d584d8bfb964242
Ehab Borae Hassanien <68748080+ehaborae@users.noreply.github.com>
2022-03-23 20:41:33 +0200

    a

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,6 @@
-import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Modal, Image } from 'react-native';
 import { useState } from 'react';
+import { color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 function GoalInput(props) {
     // register a new state here
@@ -27,15 +28,16 @@ function GoalInput(props) {
     return (
         <Modal visible={props.visible} animationType='slide' >
             <View style={style.inputContainre}>
+                <Image style={style.imageCotainer} source={require('../assets/images/favicon.png')} />
                 <TextInput placeholder='Yor cours egoal!' style={style.textInput} onChangeText={goalInputHandler} value={enteredGoalText} />
                 <View style={style.buttonsContainer}>
                     <View style={style.button}>
-                        <Button title='Add Goal' onPress={addGoalHandler} />
+                        <Button title='Add Goal' onPress={addGoalHandler} color ={'green'}/>
                     </View>
                     <View style={style.space16}></View>
 
                     <View style={style.button}>
-                        <Button title='cancel' onPress={cancelGoalHandler} />
+                        <Button title='cancel' onPress={cancelGoalHandler} color ={'red'}/>
                     </View>
                 </View>
             </View>
@@ -53,17 +55,20 @@ const style = StyleSheet.create(
             alignItems: 'center',
             paddingHorizontal: 16,
             justifyContent: 'center',
-            paddingBottom: 30,
-            borderBottomWidth: 1,
-            marginBottom: 30,
-            borderColor: '#cccccc',
+            paddingBottom: 16,
+            backgroundColor :'#311B6B',
+        
         },
         textInput: {
             marginHorizontal: 16,
-            padding: 4,
+            paddingHorizontal: 12,
+            padding : 6,
             width: '100%',
+            color: '#311B6B',
+            borderRadius: 6,
             borderWidth: 1,
             borderColor: '#cccccc',
+            backgroundColor: '#cccccc',
         },
         buttonsContainer: {
             marginTop: 16,
@@ -76,6 +81,11 @@ const style = StyleSheet.create(
         },
         space16: {
             width: 16,
+        },
+        imageCotainer: {
+            width: 50,
+            height: 50,
+            marginBottom: 16,
         },
 
     }
