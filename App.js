bc1c7101c71654a36c2c485179276bd7bc97e9c5
Ehab Borae Hassanien <68748080+ehaborae@users.noreply.github.com>
2022-03-24 09:16:49 +0200

    Squashed commit of the following:
    commit d5e6ecdceb40089dae8d9b15ecbb84d8e5d3d5c3
    Author: Ehab Borae Hassanien <68748080+ehaborae@users.noreply.github.com>
    Date:   Thu Mar 24 09:15:44 2022 +0200
    
        a

--- App.js
+++ App.js
@@ -1,102 +1,16 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, ScrollView, FlatList, Button } from 'react-native';
-
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
-
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
 
 
-  const [modalIsVisible, setModalVisibility] = useState(false);
-
-  function startAddGoalHandeler() {
-    setModalVisibility(true);
-  }
-
-  function closeModalHandeler() {
-    setModalVisibility(false);
-  }
-
-  const [courseGoals, setCoursGoals] = useState([]);
-  // this function is to handel get data and submiting it
-  function addGoalHandler(enteredGoalText) {
-    setCoursGoals((currentCourseGoals) => [...courseGoals, { text: enteredGoalText, id: Math.random().toString() }]);
-  };
-
-
-  function deleteGoalHandeler(id) {
-    setCoursGoals(currentCourseGoals => {
-      return currentCourseGoals.filter((goal) => goal.id !== id);
-    });
-  }
-
-
   return (
-    <View style={style.appContainer}>
-
-
-      <Button title='Add new Goal' color='#5e0acc' onPress={startAddGoalHandeler} />
-
-
-      {modalIsVisible && <GoalInput onAddGoal={addGoalHandler} visible={modalIsVisible} cancelGoalHandler={closeModalHandeler} />}
-
-      <View style={style.goalsContainer}>
-        <FlatList data={courseGoals} renderItem={itemData => {
-
-          return (
-            <GoalItem
-              text={itemData.item.text}
-              id={itemData.item.id}
-              onDeleteItem={deleteGoalHandeler}
-            />
-          );
-        }} keyExtractor={(item, index) => {
-          return item.id;
-        }} />
-      </View>
+    <View style={
+      {padding:20}
+    }>
+      <Text>
+        Ehab
+        </Text>
     </View>
   );
 }
-
-const style = StyleSheet.create({
-  appContainer: {
-    paddingTop: 50,
-    paddingHorizontal: 30,
-    // height:'100%',
-    flex: 1,
-    width: '100%'
-
-  },
-  inputContainre: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingBottom: 30,
-    borderBottomWidth: 1,
-    flex: 1,
-    marginBottom: 30,
-    borderColor: '#cccccc',
-  },
-  textInput: {
-    padding: 4,
-    paddingLeft: 8,
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    flex: 1,
-    marginRight: 8,
-  },
-  goalsContainer: {
-    flex: 5,
-  },
-  goalItem: {
-    padding: 8,
-    margin: 8,
-    color: 'white',
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  goalTextItem: {
-    color: 'white',
-  }
-});
