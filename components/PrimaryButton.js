9dcc0b40bcc5d26f8af45f03bbe203561b2b2bc9
Ehab Borae Hassanien <68748080+ehaborae@users.noreply.github.com>
2022-03-24 16:14:32 +0200

    Squashed commit of the following:
    commit 1b9cb7346bfe3793eb0b9ad65ffcfb93be3f3044
    Author: Ehab Borae Hassanien <68748080+ehaborae@users.noreply.github.com>
    Date:   Thu Mar 24 16:13:08 2022 +0200
    
        target app ui
    
    commit bc1c7101c71654a36c2c485179276bd7bc97e9c5
    Author: Ehab Borae Hassanien <68748080+ehaborae@users.noreply.github.com>
    Date:   Thu Mar 24 09:16:49 2022 +0200
    
        Squashed commit of the following:
    
        commit d5e6ecdceb40089dae8d9b15ecbb84d8e5d3d5c3
        Author: Ehab Borae Hassanien <68748080+ehaborae@users.noreply.github.com>
        Date:   Thu Mar 24 09:15:44 2022 +0200
    
            a

---
+++ components/PrimaryButton.js
@@ -0,0 +1,40 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+function PrimaryButton({ children }) {
+
+    function pressHandeler() {
+        console.log('Pressed');
+    }
+
+    return (
+        <View style={style.buttonOuterContariner}>
+            <Pressable onPress={pressHandeler} android_ripple={{ color: '#640233' }} style={({ pressed }) => pressed ? [style.buttonInnerContariner, style.pressed] : style.buttonInnerContariner}>
+                <Text style={style.buttonText}>{children}</Text>
+            </Pressable>
+        </View>
+    );
+}
+
+export default PrimaryButton;
+
+const style = StyleSheet.create({
+
+    buttonOuterContariner: {
+        overflow: 'hidden',
+        borderRadius: 8,
+        margin: 4,
+    },
+    buttonInnerContariner: {
+        backgroundColor: '#72063c',
+        elevation: 2,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+        marginVertical: 12,
+        marginHorizontal: 24,
+    },
+    pressed: {
+        opacity: 0.75,
+    },
+});
